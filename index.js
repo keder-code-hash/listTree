bed61f27f4e5070b2f6f6a6fb0b8c142b0da12e7
keder-code-hash <kedernath.mallick.tint022@gmail.com>
2021-08-30 22:54:56 +0530

    f_c_1

--- index.js
+++ index.js
@@ -17,11 +17,32 @@ const year_no=3;
 const month_no=12;
 const month=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
+function dataGen(months){
+    this.month=months;
+}
+var data=new dataGen(month);
 
-function toggling()
+
+// function toggling(togglingElem,bolckIngElem)
+// {
+//     var toggling=document.getElementById('toggle');
+//     var postAct=document.getElementById('postdata');
+//     if(toggling.className=='toogle-closed'){
+//         toggling.setAttribute('class','toogle-open');
+//         toggling.firstChild.innerHTML="▼&nbsp";
+//         postAct.style.display='block';
+//     }
+//     else{
+//         toggling.setAttribute('class','toogle-closed');
+//         toggling.firstChild.innerHTML="►&nbsp";
+//         postAct.style.display='none';
+//     }
+// }
+function toggling(togglingNode,bolckIngElem)
 {
-    var toggling=document.getElementById('toggle');
-    var postAct=document.getElementById('postdata');
+    
+    var toggling=document.getElementById('toggleId');
+    var postAct=document.getElementById('postAdd');
     if(toggling.className=='toogle-closed'){
         toggling.setAttribute('class','toogle-open');
         toggling.firstChild.innerHTML="▼&nbsp";
@@ -33,47 +54,64 @@ function toggling()
         postAct.style.display='none';
     }
 }
+
+function toggleBlockGen(postid,filterName,postcount)
+{
+    var parentNode=document.createElement('ul');
+    parentNode.setAttribute('class','parentElem');
+
+    var parentExpand=document.createElement('li');
+    parentExpand.setAttribute('class','parentExpanded');
+
+    var togglingPNode=document.createElement('a');
+    togglingPNode.setAttribute('class','toggle');
+    togglingPNode.setAttribute('id','toggleId');
+    togglingPNode.setAttribute('onclick','toggling()');
+
+    var spanToggleSign=document.createElement('span');
+    spanToggleSign.setAttribute('class','toogle-closed');
+    spanToggleSign.innerHTML="►&nbsp";
+    // spanToggleSign.innerHTML="▼&nbsp";
+
+    var PostFiltertType=document.createElement('a');
+    PostFiltertType.setAttribute('class','postLink filterType');
+    PostFiltertType.setAttribute('href','post:'+postid);
+    PostFiltertType.innerHTML=filterName;
+
+    var PostCount=document.createElement('span');
+    PostCount.setAttribute('class','post-count');
+    PostCount.innerHTML=postcount;
+
+    //if the parent node is for year themn we have to add again a parentNode for months.
+    //if the parent Node is for month then we have to add again a childNode of postaList
+    // var PostList=document.createElement('ul');
+    // PostList.setAttribute('class','posts');
+    // PostList.setAttribute('id','postAdd');
+
+    togglingPNode.appendChild(spanToggleSign);
+
+    parentExpand.append(togglingPNode,PostFiltertType,postcount);
+
+    parentNode.append(parentExpand);
+
+    return parentNode;
+}
+
 window.onload=()=>{
-    console.log(month);
+    console.log(data.month);
     const parent=document.getElementById('testList');
-    let text=document.createElement("p");
 
-    var ulDom=document.createElement('ul');
-
-    var li1=document.createElement('li');
+    const newBlock=toggleBlockGen('none',2019,19);
     
-    var toogleTag=document.createElement('a');
-    toogleTag.setAttribute('id','toggle');
-    toogleTag.setAttribute('class','toggle-closed');
-    
-    toogleTag.setAttribute('onclick','toggling()');
 
-    var spanSignDom=document.createElement('span');
-    // spanSignDom.setAttribute('id','togSign');
-    spanSignDom.innerHTML="►&nbsp";
-    
-    toogleTag.appendChild(spanSignDom);
-    
-    var yearNo=document.createElement('a');
-    yearNo.setAttribute('href','YearWiseFilter:'+2019);
-    yearNo.innerText="2019";
-
-    var spanPostCount=document.createElement('span');
-    spanPostCount.innerHTML='(16)';
-
-    li1.append(toogleTag,yearNo,spanPostCount);
-
-    var li2=document.createElement('li');
-    li2.setAttribute('id','postdata');
     for(var count=0;count<obj[2019].length;count++)
     {
         if(obj[2019][count].length>0)
         {       
-            var monthUl=document.createElement('ul');
-            var month=document.createElement('a');
-            month.innerText=month[count];
-            month.setAttribute('href','monthWisePost:postid');
+            const newMonth=toggleBlockGen('none',data.month[count],obj[2019][count].length);
 
+            var monthUl=document.createElement('ul');
+            
             for(let post=0;post<obj[2019][count].length;post++){
 
                 var liDom=document.createElement('li');
@@ -81,13 +119,16 @@ window.onload=()=>{
                 atag.setAttribute('href','post:'+obj[2019][count][post]);
                 atag.innerHTML="post Title";
                 liDom.appendChild(atag);
-                month.appendChild(liDom);
-            }
-        }
-        li2.appendChild(month);
-    }
-    ulDom.append(li1,li2);
+                monthUl.appendChild(liDom);
 
-    text.textContent="Hii this is working";
-    parent.appendChild(ulDom);
+            }
+
+            newMonth.firstChild.appendChild(monthUl);
+
+            newBlock.firstChild.append(newMonth);
+        }
+        
+    }
+    
+    parent.appendChild(newBlock);
 }
